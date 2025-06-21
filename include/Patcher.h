31c162634c2610be691bc5030d683a712f72ec6c
Nik0lyaZ <nicknekrasov09@gmail.com>
2025-06-21 21:57:20 +0300

    Refactor code, add return codes enum

--- include/Patcher.h
+++ include/Patcher.h
@@ -2,21 +2,26 @@
 #define PATCHER_H
 #include <iostream>
 
-class Patcher
-{
-    public:
-        int Patch(std::string filename, int ver, unsigned obj, int lib = 0, unsigned approx = 0, bool visual = false);
-        static unsigned Approx(unsigned obj, bool type = 0);
-        static int GetAutoVer(std::string filename);
+enum class Result {
+    OK, LibWrong, LibInvalid, VerInvalid, PfmInvalid, ApproxReq, FileError
 };
 
-/* ### PATCHER ERROR CODES ###
-    0 - invalid library for specified version
-    1 - success
-    2 - approximation problem
-    3 - file could not be openned
-    4 - invalid library
-    5 - invalid version
+class Patcher {
+public:
+    Result Patch(std::string filename, int ver, unsigned obj, int lib = 0, unsigned approx = 0, bool visual = false);
+    static unsigned Approx(unsigned obj, bool type = 0);
+    static int GetAutoVer(std::string filename);
+};
+
+/*
+### PATCHER RETURN CODES ###
+    1. OK - you are *probably* good
+    2. Wrong library (e.g. armeabi-v7a for 1.41 or x86 for 1.00)
+    3. Invalid library - nonexistent ID for library (any ID outside the range 0-2)
+    4. Invalid version - pretty much the same as LibInvalid but for version IDs
+    5. Invalid platform - upcoming feature...
+    6. Requested approximation question - needed for the corresponding dialog
+    7. File error - the file couldn't be opened...
 */
 
 #endif // PATCHER_H
